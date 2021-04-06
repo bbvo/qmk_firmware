610bf0f56ae03e568c93896edd533286820059b9
bbvo <77623320+bbvo@users.noreply.github.com>
2021-04-06 22:46:17 +0900

    keymap.c changed

--- keyboards/ut472/keymaps/custom-bbvo/keymap.c
+++ keyboards/ut472/keymaps/custom-bbvo/keymap.c
@@ -28,55 +28,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------------------+
    * |Tab/L3|  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |   '  |
    * |-------------------------------------------------------------------------+
-   * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Sh/En|
+   * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  Up  |Sh/En|
    * |-------------------------------------------------------------------------+
-   * | Ctrl| App | Gui | Alt |  L2  |   Space   |  L1  | Left| Down|  Up |Right|
+   * | Ctrl| App | Gui | Alt |  L2  |   Space   |  L1  |  /  | Left| Down |Right|
    * `-------------------------------------------------------------------------'
    */
 
 LAYOUT(
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LGUI, KC_APP, KC_LCTL, KC_LALT,  MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT,
+  KC_LGUI, KC_APP, KC_LCTL, KC_LALT,  MO(2),      KC_SPC,        MO(1),  KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
   /* FN Layer 1
    * ,-------------------------------------------------------------------------.
    * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
    * |-------------------------------------------------------------------------+
-   * |      | F11 | F12 |     |     |     |     |  -  |  =  |  [  |  ]  |  \   |
+   * |      |     |     |     |     |     |     |  -  |  =  |  [  |  ]  |  \   |
    * |-------------------------------------------------------------------------+
-   * |       | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 |     |
+   * |       |     |     |     |     |     |     |     |     |     | PgUp|     |
    * |-------------------------------------------------------------------------+
-   * |     |     |     |Capsl|      |          |       | Home| PgDn| PgUp| End |
+   * |     |     |     |Capsl|      |          |       |     | Home| PgDn| End |
    * `-------------------------------------------------------------------------'
    */
 
 LAYOUT( /* Right */
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
-  _______, KC_F11,  KC_F12,  _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,  _______,
+  _______, _______, _______, KC_CAPS, _______,     _______,      _______, _______, KC_HOME, KC_PGDN, KC_END
 ),
 
   /* FN Layer 2
    * ,-------------------------------------------------------------------------.
    * |   ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |Delete|
    * |-------------------------------------------------------------------------+
-   * |      |     |     |     |     |     |     |  _  |  +  |  { |  }  |   |   |
+   * |      | F8  | F9  | F10 | F11 | F12 |     |  _  |  +  |  { |  }  |   |   |
    * |-------------------------------------------------------------------------+
-   * |       |     |     |     |     |     |     |     |     |     |     |     |
+   * |       | F1  | F2  | F3  | F4  | F5  | F6  | F7  |     |     | PgUp|     |
    * |-------------------------------------------------------------------------+
-   * |     |     |     |Capsl|      |          |       | Home| PgDn| PgUp| End |
+   * |     |     |     |Capsl|      |          |       |     | Home| PgDn| End |
    * `-------------------------------------------------------------------------'
    */
 
 LAYOUT( /* Left */
-  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-  _______, _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  KC_TILDE, KC_EXCLAIM, KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND,  KC_ASTERISK, KC_LEFT_PAREN,       KC_RIGHT_PAREN,       KC_DELETE,
+  _______,    KC_F8,    KC_F9,  KC_F10,  KC_F11,    KC_F12,      _______,       KC_UNDERSCORE, KC_PLUS,     KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
+  _______,    KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,      KC_F6,         KC_F7,         _______,     _______,             KC_PGUP,              _______,
+  _______,   _______,  _______, KC_CAPS,   _______,    _______,            _______,       _______,     KC_HOME,             KC_PGDN,              KC_END
 ),
 
   /* FN Layer 2
